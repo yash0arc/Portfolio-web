aaf0f5c5d05df604ff0426364f15004c01b39fbc
Yashwanth Sai Anumula <yashwanthsai444@gmail.com>
2025-10-22 22:37:00 +0530

    removed src from the link in the resume jsx

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -1,17 +1,14 @@
 import "./resume.css";
-
 function Resume() {
   const handleDownload = () => {
-    window.open("/files/Ayashwanthsaianumula_resume.pdf", "_blank");
+    window.open("/files/yashwanthsaianumula_resume.pdf", "_blank");
   };
-
   return (
     <div className="resume">
       <button onClick={handleDownload} className="resumebtn">
-        View / Download Resume
+        Click Here
       </button>
     </div>
   );
 }
-
 export default Resume;
